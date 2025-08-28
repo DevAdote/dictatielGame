7c7b79afb837cb9404755e9b91558d1aaddf49e6
DevAdote <elieabeladote@gmail.com>
2025-08-28 16:53:04 +0200

    utilisation de fonction

---
+++ scripts/script.js
@@ -0,0 +1,47 @@
+function afficherResultat (score, nbMotsProposes){
+        console.log('Vous avez reussi '+score+ ' sur ' +nbMotsProposes)
+
+}
+
+
+function choisirPhrasesOuMots(){
+    let choix = prompt('Choisir entre mots et phrases')
+    while(choix!=='mots' && choix!=='phrases')
+    {
+          choix = prompt('Vous devez choisir entre mots ou phrases')
+    }
+    return choix
+}
+
+
+
+function lancerBoucleDeJeu (listePropositions){
+    
+  
+        for(let i = 0; i<listePropositions.length; i++)
+            {
+                let motUtilisateur = prompt('Saisir le mot suivant : ' +listePropositions[i])
+                
+                if(motUtilisateur === listePropositions[i]){
+                    score++
+                }
+            }
+  
+        return score;
+}
+
+
+
+function lancerJeu (){
+   let choix = choisirPhrasesOuMots()
+   let score = 0
+   let nbMotsProposes = 0
+   if (choix === 'mots'){
+    lancerBoucleDeJeu(listeMots)
+    nbMotsProposes = listeMots.length
+   }else{
+     lancerBoucleDeJeu(listePhrases)
+    nbMotsProposes = listePhrases.length
+   }
+    afficherResultat(score, nbMotsProposes)
+}
