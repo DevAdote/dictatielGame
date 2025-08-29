f551813c932e9e78c028dbf3d1c36b64cf5bdc87
DevAdote <elieabeladote@gmail.com>
2025-08-29 12:01:21 +0200

    Selection avec les methodes getElementById ...>

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,20 @@
+// Recuperation de l'id du bouton de validation et de l'input d'ecriture
+
+let inputEcriture = document.getElementById("inputEcriture")
+
+let btnValiderMot = document.getElementById("btnValiderMot")
+
+// Selection du mot propose et du score
+
+let zoneProposition = document.querySelector(".zoneProposition")
+
+let scoreAffiche = document.querySelector(".zoneScore span")
+
+
+// Selection des boutons radio de choix
+
+let optionChoix = document.querySelectorAll(".optionSource input")
+
+
+
 lancerJeu()
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 function afficherResultat (score, nbMotsProposes){
-        console.log('Vous avez reussi '+score+ ' sur ' +nbMotsProposes)
+        console.log('Vous avez reussi '+ score + ' sur ' +nbMotsProposes)
 
 }
 
@@ -17,7 +17,7 @@ function choisirPhrasesOuMots(){
 
 function lancerBoucleDeJeu (listePropositions){
     
-  
+        let score = 0
         for(let i = 0; i<listePropositions.length; i++)
             {
                 let motUtilisateur = prompt('Saisir le mot suivant : ' +listePropositions[i])
@@ -37,10 +37,10 @@ function lancerJeu (){
    let score = 0
    let nbMotsProposes = 0
    if (choix === 'mots'){
-    lancerBoucleDeJeu(listeMots)
+    score = lancerBoucleDeJeu(listeMots)
     nbMotsProposes = listeMots.length
    }else{
-     lancerBoucleDeJeu(listePhrases)
+     score = lancerBoucleDeJeu(listePhrases)
     nbMotsProposes = listePhrases.length
    }
     afficherResultat(score, nbMotsProposes)
